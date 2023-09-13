0342caa30703ccb006f66d66787e3c5271df1d29
Evans Mike <etnperlong@gmail.com>
2023-09-13 16:06:07 +0100

    fix: error when upgrading to 23.05

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -190,7 +190,7 @@ rebuild_firmware() {
         docker-compose dockerd dosfstools dumpe2fs e2freefrag e2fsprogs exfat-mkfs \
         f2fs-tools f2fsck fdisk gawk getopt gzip hostapd-common iconv iw iwinfo jq jshn \
         kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script \
-        liblucihttp liblucihttp-lua libnetwork losetup lsattr lsblk lscpu mkf2fs \
+        liblucihttp liblucihttp-lua losetup lsattr lsblk lscpu mkf2fs \
         mount-utils openssl-util parted perl-http-date perlbase-file perlbase-getopt \
         perlbase-time perlbase-unicode perlbase-utf8 pigz ppp ppp-mod-pppoe \
         proto-bonding pv rename resize2fs runc subversion-client subversion-libs tar \
@@ -198,7 +198,7 @@ rebuild_firmware() {
         uuidgen wget-ssl whereis which wpad-basic wwan xfs-fsck xfs-mkfs xz \
         xz-utils ziptool zoneinfo-asia zoneinfo-core zstd \
         \
-        luci luci-base luci-compat luci-i18n-base-en luci-i18n-base-zh-cn luci-lib-base  \
+        luci luci-base luci-compat luci-i18n-base-zh-cn luci-lib-base  \
         luci-lib-docker luci-lib-ip luci-lib-ipkg luci-lib-jsonc luci-lib-nixio  \
         luci-mod-admin-full luci-mod-network luci-mod-status luci-mod-system  \
         luci-proto-3g luci-proto-bonding luci-proto-ipip luci-proto-ipv6 luci-proto-ncm  \
